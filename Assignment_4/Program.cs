740e66f31f973ca05fa2bcac9454025804cb2263
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-06 11:47:26 +0300

    Question 2

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -4,10 +4,19 @@
     {
         static void Main(string[] args)
         {
-            #region Question 1
-            double[] price = { 25.5, 40.0, 33.75 };
-            Console.WriteLine(price[1]);
-            #endregion
+            //#region Question 1
+            //double[] price = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(price[1]);
+            //#endregion
+
+            //#region Question 2
+            //int[,] shelfCopies = {
+            //{ 3, 5 },
+            //{ 1, 4 }
+            //              };
+            //Console.WriteLine(shelfCopies[1, 0]);
+
+            //#endregion
         }
     }
 }
